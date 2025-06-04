0469d417f02fc2b4b0559ba41001bda7df9e3ef8
Onesmus Mutyauvyu <89151741+Onesmus123@users.noreply.github.com>
2025-06-04 15:15:56 +0300

    My first NodeJS Application

---
+++ intro.js
@@ -0,0 +1,18 @@
+// Node.js uses JavaScript on the server
+// Node.js uses asynchronous programming!
+// Node.js eliminates the waiting, and simply continues with the next request.
+
+const http = require('http'); // Create HTTP instance
+
+const port = 3000;
+
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end("Hello World! This is my first NodeJS application!");
+}).listen(port, (error) => {
+    if (error) {
+        console.error("Error starting server:", error);
+    } else {
+        console.log(`My first NodeJS app is running on: http://localhost:${port}`);
+    }
+});
